07074bfdcf82620c11d9c658af3de11a80c804dc
shixiao <shixiao@pp100.com>
2017-08-09 18:14:36 +0800

    weixinspider

---
+++ Crawler/weixin/weixinSpider.py
@@ -0,0 +1,124 @@
+# coding:utf-8
+
+from urllib.parse import urlencode
+import pymongo
+import requests
+from lxml.etree import XMLSyntaxError
+from requests.exceptions import ConnectionError
+from pyquery import PyQuery as pq
+from weixinConfig import *
+
+base_url = 'http://weixin.sogou.com/weixin?'
+
+headers = {
+    'Cookie': 'IPLOC=CN4403; SUV=002F65A0B70E119F58C8D9A888B1B914; SUID=B7100EB73320910A0000000058E8B823; ABTEST=0|1502250908|v1; SNUID=2CEFF34AFDF8AAB6F73ADA80FD1FFC25; weixinIndexVisited=1; sct=2; JSESSIONID=aaahk-rqk9YjT-F8xZC2v',
+    'Host': 'weixin.sogou.com',
+    'Referer': 'http://weixin.sogou.com/',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+}
+
+proxy = None
+
+def get_proxy():
+    try:
+        response = requests.get(PROXY_POOL_URL)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except ConnectionError:
+        return None
+
+def get_html(url, count=1):
+    print('crawimg:', url)
+    print('trying:', count)
+    global proxy
+    if count >= MAX_COUNT:
+        print('tried too many counts')
+        return None
+    try:
+        if proxy:
+            proxies = {
+                'http': 'http://' + proxy
+            }
+            response = requests.get(url, allow_redirects= False,headers= headers,proxies= proxies)
+        else:
+            response = requests.get(url, allow_redirects= False,headers= headers)
+        if response.status_code == 200:
+            return response.text
+        if response.status_code == 302:
+            print('302')
+            proxy = get_proxy()
+            if proxy:
+                print('using proxy',proxy)
+                return get_html(url)
+            else:
+                print('get proxy failed')
+                return None
+    except ConnectionError as e:
+        print('error occurred',e.args)
+        proxy = get_proxy()
+        proxy += 1
+        return get_html(url,count)
+
+def get_index(keyword,page):
+    data = {
+        'query':keyword,
+        'type':2,
+        'page':page
+    }
+    queries = urlencode(data)
+    url = base_url + queries
+    html = get_html(url)
+    return html
+
+def parse_index(html):
+    doc = pq(html)
+    items = doc('.news-box .news-list li .txt-box h3 a').items()
+    for item in items:
+        yield item.attr('href')
+
+def get_detail(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except ConnectionError:
+        return None
+
+def parse_detail(html):
+    try:
+        doc = pq(html)
+        title = doc('.rich_media_title').text()
+        content = doc('.rich_media_content').text()
+        date = doc('#post-date').text()
+        nickname = doc('#js_profile_qrcode > div > strong').text()
+        wechat = doc('#js_profile_qrcode > div > p:nth-child(3) > span').text()
+        return {
+            'title': title,
+            'content': content,
+            'date': date,
+            'nickname': nickname,
+            'wechat': wechat
+        }
+    except XMLSyntaxError:
+        return None
+
+def save_to_mongo(data):
+    print(data)
+
+def main():
+    for page in range(1, 101):
+        html = get_index(KEYWORD,page)
+        if html:
+            article_urls = parse_index(html)
+            for article_url in article_urls:
+                article_html = get_detail(article_url)
+                if article_html:
+                    article_data = parse_detail(article_html)
+                    if article_data:
+                        save_to_mongo(article_data)
+
+if __name__ == '__main__':
+    main()
