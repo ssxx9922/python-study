2c1bb1a76561fe4cc27cb0f59d4067d6123930d3
shixiao <shixiao@pp100.com>
2017-12-07 15:19:56 +0800

    change login

--- chinaMobile/test.py
+++ chinaMobile/test.py
@@ -47,35 +47,23 @@ def login(mobile, code,cookies):
         'continue':'http://www.bj.10086.cn/my',
         'style':'BIZ_LOGINBOX',
 		'service':'www.bj.10086.cn',
-		'box':'',
         'ssoLogin':'yes',
         'loginMode': 2,
         'loginMethod': 1,
         'loginName': mobile,
         'target':'_parent',
-		'password':'',
         'smsNum': code,
-        'rnum':'',
         'ckCookie': 'on',
     }
     headers = {
-        'Host':'bj.ac.10086.cn',
-		'Connection':'keep-alive',
-		'Content-Length':'285',
-		'Cache-Control':'max-age=0',
-		'Origin':'https://bj.ac.10086.cn',
-		'Upgrade-Insecure-Requests':'1',
 		'Content-Type':'application/x-www-form-urlencoded',
 		'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
 		'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-		'Referer':'https://bj.ac.10086.cn/ac/cmsso/iloginnew.jsp',
-		'Accept-Encoding':'gzip, deflate, br',
-		'Accept-Language':'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7',
     }
     response = requests.post(url, data=data, headers=headers, cookies=cookies, allow_redirects=False)
     print('北京移动，登录：', mobile, response.status_code, response.headers)
 
-    newurl = response.headers['Location']
+    newurl = 'http' + response.headers['Location'][5:]
     response1 = requests.get(newurl, cookies=cookies)
     print('北京移动，登录第二步：', mobile, response1.status_code, response1.headers)
 
