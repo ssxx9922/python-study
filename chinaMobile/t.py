09d1fa0e40578978a78109d981be8af80c37c4aa
shixiao <shixiao@pp100.com>
2017-12-22 18:41:58 +0800

    china mobile

--- chinaMobile/t.py
+++ chinaMobile/t.py
@@ -15,6 +15,35 @@ import json
 # cookies['ssologinprovince'] = newResponse.cookies['ssologinprovince']
 # print(cookies)
 
-cookies = {'is_login': 'true', 'userinfokey': '%7b%22loginType%22%3a%2201%22%2c%22provinceName%22%3a%22100%22%2c%22pwdType%22%3a%2201%22%7d', 'verifyCode': '4d7ce286a2e40576b33de45db42ab5db68209376', 'c': '4c18f34e87bc45b4adc2129a1800c303', 'jsessionid-echd-cpt-cmcc-jt': 'F9A8D74650C9D3B28254D261540041F9', 'ssologinprovince': '100'}
-print(type(cookies))
-print(cookies)
+import re
+import base64
+
+result = 'jQuery18308124502347056595_1513071820224({"data":null,"retCode":"000000","retMsg":"success","sOperTime":null})'
+s = re.search('({.*?})',result,re.S)
+i = s.group()
+print(json.loads(i)['retCode'])
+
+
+print(base64.b64encode('079230'.encode('utf-8')).decode())
+
+# ============================
+
+GET /i/v1/fee/detailbillinfojsonp/15011463580?callback=jQuery1830615989468084053_1513759759587&curCuror=1&step=100&qryMonth=201711&billType=02&_=1513759888880 HTTP/1.1
+Host: shop.10086.cn
+Connection: keep-alive
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36
+Accept: */*
+Referer: http://shop.10086.cn/i/?f=billdetailqry
+Accept-Encoding: gzip, deflate, br
+Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7
+Cookie: ssologinprovince=100; collect_id=9pt9xruastbi31nir4wo5lvrwsvmo5ml; jsessionid-echd-cpt-cmcc-jt=90E57240A6482F0981F3E7027BF5B4A5; CmLocation=100|100; CmProvid=bj; cmccssotoken=f1640cbd8d6542ffa81cfa8a27d1d182@.10086.cn; is_login=true; userinfokey=%7b%22loginType%22%3a%2201%22%2c%22provinceName%22%3a%22100%22%2c%22pwdType%22%3a%2201%22%2c%22userName%22%3a%2215011463580%22%7d; c=f1640cbd8d6542ffa81cfa8a27d1d182; verifyCode=4d7ce286a2e40576b33de45db42ab5db68209376; WT_FPC=id=254f49c981fb5a23ab81513759453605:lv=1513759759682:ss=1513759453605
+
+
+GET /i/v1/fee/detailbillinfojsonp/15011463580?callback=jQuery18309115520519448623_1513669304676&curCuror=1&step=100&qryMonth=201711&billType=02&_=1513761018418 HTTP/1.1
+Host: shop.10086.cn
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36
+Accept-Encoding: gzip, deflate, br
+Accept: */*
+Connection: keep-alive
+Referer: http://shop.10086.cn/i/?f=billdetailqry
+Cookie: ssologinprovince=100; collect_id=9pt9xruastbi31nir4wo5lvrwsvmo5ml; jsessionid-echd-cpt-cmcc-jt=90E57240A6482F0981F3E7027BF5B4A5; CmLocation=100|100; CmProvid=bj; cmccssotoken=f1640cbd8d6542ffa81cfa8a27d1d182@.10086.cn; is_login=true; userinfokey=%7b%22loginType%22%3a%2201%22%2c%22provinceName%22%3a%22100%22%2c%22pwdType%22%3a%2201%22%2c%22userName%22%3a%2215011463580%22%7d; c=f1640cbd8d6542ffa81cfa8a27d1d182; verifyCode=4d7ce286a2e40576b33de45db42ab5db68209376; WT_FPC=id=254f49c981fb5a23ab81513759453605:lv=1513759759682:ss=1513759453605
--- chinaMobile/test1.py
+++ chinaMobile/test1.py
@@ -4,6 +4,17 @@ import requests
 import time
 import json
 import random
+import base64
+import re
+import math
+
+def get_w():
+    co_f='2'
+    curt=str(int(time.time()*1000))
+    for i in range(1,32-len(curt)):
+	    co_f+=hex(math.floor(random.random()*16))[-1]
+    co_f+=curt
+    return 'id=' + co_f + ':lv=' + curt + ':ss=' + curt
 
 #  登录-发验证码
 def get_verify_code_image(mobile):
@@ -59,21 +70,26 @@ def login(mobile,pwd,smsPwd):
         'Referer': 'https://login.10086.cn/html/window/loginMini.html?channelID=12003&backUrl=http://shop.10086.cn/i/sso.html',
     }
     print(url)
-    response = requests.get(url,headers=headers,verify=False)
+    response = requests.get(url,headers=headers,verify=False,allow_redirects=False)
     print(response.status_code,response.text)
-    print(response.headers)
+    print('重定向之前的cookies =>',response.cookies)
     result = json.loads(response.text)
     cookies = {}
-    cookies['is_login'] = response.headers['is_login']
-    cookies['userinfokey'] = response.headers['userinfokey']
-    cookies['verifyCode'] = response.headers['verifyCode']
-    cookies['c'] = response.headers['c']
-    newUrl = result['assertAcceptURL'] + 'backUrl=http://shop.10086.cn/i/sso.html&artifact=' + result['artifact']
+    cookies.update(response.cookies)
+    # cookies['is_login'] = response.cookies['is_login']
+    # cookies['userinfokey'] = response.cookies['userinfokey']
+    # cookies['verifyCode'] = response.cookies['verifyCode']
+    # cookies['c'] = response.cookies['c']
+    newUrl = result['assertAcceptURL'] + '?backUrl=http://shop.10086.cn/i/sso.html&artifact=' + result['artifact']
     newResponse = requests.get(newUrl,cookies=cookies,verify=False,allow_redirects=False)
-    print(newResponse.cookies)
-    cookies['jsessionid-echd-cpt-cmcc-jt'] = newRespons.cookies['jsessionid-echd-cpt-cmcc-jt']
-    cookies['ssologinprovince'] = newResponse.cookies['ssologinprovince']
-
+    print('重定向请求结果 =>',newResponse.status_code)
+    print('重定向请求结果 =>',newResponse.cookies)
+    # cookies['jsessionid-echd-cpt-cmcc-jt'] = newResponse.cookies['jsessionid-echd-cpt-cmcc-jt']
+    # cookies['ssologinprovince'] = newResponse.cookies['ssologinprovince']
+    # cookies['WT_FPC'] = get_w()
+    # cookies['CmProvid'] = 'bj'
+    # cookies['cmccssotoken'] = response.cookies['cmccssotoken']
+    cookies.update(newResponse.cookies)
     return cookies
 
 # 获取账单时的 图形验证码
@@ -90,39 +106,110 @@ def get_project_verify_code(mobile,code,cookies):
     url = 'http://shop.10086.cn/i/v1/res/precheck/' + mobile + '?captchaVal=' + code + '&_=' + str(int(time.time()*1000))
     response = requests.get(url,cookies=cookies)
     result = json.loads(response.text)
-    print('验证码是否正确  =>','正确' if result['retCode'] == '000000' else '错误')
+    print('验证码是否正确  =>','正确' if result['retCode'] == '000000' else '失败')
 
 # 获取账单时的 发送短信验证码
 def get_project_send_code(mobile,cookies):
-    url = 'https://shop.10086.cn/i/v1/fee/detbillrandomcodejsonp/' + mobile + '?callback=jQuery18307393072971157502_1512793409095&_=' + str(int(time.time()*1000))
-    response = requests.get(url,cookies=cookies,verify=False)
+    url = 'https://shop.10086.cn/i/v1/fee/detbillrandomcodejsonp/' + mobile + '?callback=jQuery18309115520519448623_1513669304676&_=' + str(int(time.time()*1000))
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+        'Referer': 'http://shop.10086.cn/i/?f=billdetailqry'
+    }
+    response = requests.get(url,cookies=cookies,headers=headers,verify=False)
+    print(response.status_code)
     print(response.text)
+    jsonStr = re.search('({.*?})',response.text,re.S).group()
+    result = json.loads(jsonStr)
+    print('发送验证码是否正确  =>','正确' if result['retCode'] == '000000' else '失败')
+
 
 # 获取账单时的 二次登陆
-def get_project_login(mobile,code) :
-    url = 'https://shop.10086.cn/i/v1/fee/detailbilltempidentjsonp/' + mobile + '?callback=jQuery18304613214506293093_1512798307557&pwdTempSerCode='+'MjYzNjYx'+'&pwdTempRandCode='+'MzE2MTE3'+'&captchaVal=' + code + '&_=' + str(int(time.time()*1000)) 
-    response = requests.get(url)
-    print()
+def get_project_login(mobile,pwd,smsCode,imgCode,cookies):
+    url = 'https://shop.10086.cn/i/v1/fee/detailbilltempidentjsonp/{mobile}?callback=jQuery18309115520519448623_1513669304676&pwdTempSerCode={pwd}&pwdTempRandCode={smsCode}&captchaVal={imgCode}&_={timeStr}'
+    url = url.format(mobile=mobile,pwd=base64.b64encode(pwd.encode(encoding='utf-8')).decode(),smsCode=base64.b64encode(smsCode.encode(encoding='utf-8')).decode(),imgCode=imgCode,timeStr=str(int(time.time()*1000)))
+    # data = {
+    #     'callback':'jQuery18309115520519448623_1513669304676',
+    #     'pwdTempSerCode':base64.b64encode(pwd.encode(encoding='utf-8')).decode(),#服务密码
+    #     'pwdTempRandCode':base64.b64encode(smsCode.encode(encoding='utf-8')).decode(),#短信验证码
+    #     'captchaVal':imgCode,#图形验证码
+    #     '_':str(int(time.time()*1000))
+    # }
+    headers = {
+        'Referer':'http://shop.10086.cn/i/?f=billdetailqry',
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+    }
+    response = requests.get(url,headers=headers,cookies=cookies,verify=False)
+    print('二次登录')
+    print(response.status_code)
+    print(response.text)
+    print(response.cookies)
+    jsonStr = re.search('({.*?})',response.text,re.S).group()
+    result = json.loads(jsonStr)
+    print('登录是否成功  =>','正确' if result['retCode'] == '000000' else '失败')
 
 
 # 账单接口
-def detailbillinfojsonp(mobile,month):
-    url = 'http://shop.10086.cn/i/v1/fee/detailbillinfojsonp/' + mobile + '?callback=jQuery18309695543543405936_1512465642278&curCuror=1&step=100&qryMonth=' + month + '&billType=02&_=' + str(int(time.time()*1000))
-    response = requests.get(url)
+def detailbillinfojsonp(mobile,month,billType,cookies):
+    url = 'https://shop.10086.cn/i/v1/fee/detailbillinfojsonp/{mobile}?callback=jQuery1830615989468084053_1513759759587&curCuror=1&step=100&qryMonth={month}&billType={type}&_={timeStr}'
+    url = url.format(mobile=mobile,month=month,type=billType,timeStr=str(int(time.time()*1000)))
+    # data = {
+    #     'callback':'jQuery18309115520519448623_1513669304676',
+    #     'curCuror':'1',
+    #     'step':'100',
+    #     'qryMonth':month, # 201711
+    #     'billType':'02', # 01 套餐及固话 04上网 02 通话 03 短彩信 05增值业务
+    #     '_':str(int(time.time()*1000))
+    # }
+    headers = {
+        'Referer':'http://shop.10086.cn/i/?f=billdetailqry',
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+    }
+    response = requests.get(url,headers=headers,cookies=cookies,verify=False)
+    print(response.status_code)
     print(response.text)
 
+def get_basic(mobile,cookies):
+    url = 'http://shop.10086.cn/i/v1/cust/info/' + mobile + '?_=' + str(int(time.time()*1000))
+    headers = {
+        'Referer':'http://shop.10086.cn/i/?f=billdetailqry',
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+    }
+    response = requests.get(url,headers=headers,cookies=cookies)
+    result = json.loads(response.text)
+    print(result['data'])
+
 mobile = '15011463580'
-# chick_bj_mobile(mobile)
-# send_code(mobile)
-# code = input('code=')
-# pwd = '263661'
-# cookies = login(mobile,pwd,code)
+pwd = '263661'
+
+chick_bj_mobile(mobile)
+send_code(mobile)
+code = input('code=')
+cookies = login(mobile,pwd,code)
+cookies['WT_FPC'] = get_w()
+print('第一次cookies =>',cookies)
+
+# cookies = {'CmLocation': '230|230', 'CmProvid': 'cq', 'c': 'b8e4be6cc3a14ea6a83f8cdc335f10e1', 'cmccssotoken': 'b8e4be6cc3a14ea6a83f8cdc335f10e1@.10086.cn', 'is_login': 'true', 'userinfokey': '%7b%22loginType%22%3a%2201%22%2c%22provinceName%22%3a%22230%22%2c%22pwdType%22%3a%2201%22%2c%22userName%22%3a%2215730495775%22%7d', 'verifyCode': '6c135c30f8b88c7d969e207b9ed5dfdd77ef0944', 'jsessionid-echd-cpt-cmcc-jt': '6A64A109C1C557742333613FFAAB6D44', 'ssologinprovince': '230', 'WT_FPC': 'id=221b7b0ea94bfd34ce01513755626799:lv=1513755626799:ss=1513755626799'}
+# 'CmLocation': '230|230', 'CmProvid': 'cq',
 # print(cookies)
-cookies = {'is_login': 'true', 'userinfokey': '%7b%22loginType%22%3a%2201%22%2c%22provinceName%22%3a%22100%22%2c%22pwdType%22%3a%2201%22%7d', 'verifyCode': '4d7ce286a2e40576b33de45db42ab5db68209376', 'c': '4c18f34e87bc45b4adc2129a1800c303', 'jsessionid-echd-cpt-cmcc-jt': 'F9A8D74650C9D3B28254D261540041F9', 'ssologinprovince': '100'}
+
+# time.sleep(10)
+
 
 get_project_send_code(mobile,cookies)
 get_project_verify_code_image(mobile,cookies)
 imgCode = input('图形验证码是=')
 get_project_verify_code(mobile,imgCode,cookies)
+while(True):
+    smsCode = input('短信验证码是=')
+    next = input('是否下一步 [y/n] =>')
+    if next == 'y':
+        get_project_login(mobile,pwd,smsCode,imgCode,cookies)
+    elif next == 'n':
+        get_project_send_code(mobile,cookies)
+    elif next == 'e':
+        break
+
+get_basic(mobile,cookies)
+detailbillinfojsonp(mobile,'201711','02',cookies)
 
 
