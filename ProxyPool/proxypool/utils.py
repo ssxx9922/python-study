fddd9e8bbdc282a5c7ff3870744c308cdbd65693
shixiao <shixiao@pp100.com>
2017-12-26 15:04:18 +0800

    pool

--- ProxyPool/proxypool/utils.py
+++ ProxyPool/proxypool/utils.py
@@ -2,6 +2,7 @@ import requests
 import asyncio
 import aiohttp
 from requests.exceptions import ConnectionError
+from fake_useragent import UserAgent
 import random
 
 
@@ -24,3 +25,23 @@ def get_page(url, options={}):
         print('Crawling Failed', url)
         return None
 
+class Downloader(object):
+
+    def __init__(self, urls):
+        self.urls = urls
+        self._htmls = []
+
+    async def download_single_page(self, url):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                self._html.append(await resp.text())
+
+    def download(self):
+        loop = asyncio.get_event_loop()
+        tasks = [self.download_single_page(url) for url in self.urls]
+        loop.run_until_complete(asyncio.wait(tasks))
+
+    @property
+    def htmls(self):
+        self.download()
+        return self._htmls
