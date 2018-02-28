839d5f66e4255180eb38e2fba4d6bbbd29db15ad
shixiao <shixiao@pp100.com>
2018-02-28 10:31:06 +0800

    logout

--- Django/mooc/apps/users/views.py
+++ Django/mooc/apps/users/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.hashers import make_password
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.urls import reverse
 from django.views.generic.base import View
 
 from users.forms import LoginForm, RegisterForm, FergetPwdForm, ResetPwdForm
@@ -20,6 +22,12 @@ class CustomBackend(ModelBackend):
                 return user
         except Exception as e:
             return None
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse('index'))
+
 
 class LoginView(View):
     def get(self, request):
--- Django/mooc/mooc/urls.py
+++ Django/mooc/mooc/urls.py
@@ -18,13 +18,14 @@ from django.urls import path,include
 from django.views.generic import TemplateView
 
 import xadmin
-from users.views import LoginView, RegisterView, ActiveUserView, ForgetPwdView, ResetView
+from users.views import LoginView, RegisterView, ActiveUserView, ForgetPwdView, ResetView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('xadmin', xadmin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('forgetpwd/', ForgetPwdView.as_view(), name='forget_pwd'),
     path('captcha/', include('captcha.urls')),
