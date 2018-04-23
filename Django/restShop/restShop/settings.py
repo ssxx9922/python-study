34bb1ea246b63e7ecce86b89ee8b796051821b7f
shixiao <shixiao.andy@gmail.com>
2018-04-23 18:07:04 +0800

    add data

--- Django/restShop/restShop/settings.py
+++ Django/restShop/restShop/settings.py
@@ -136,3 +136,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- Django/restShop/restShop/urls.py
+++ Django/restShop/restShop/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls import url
 from django.urls import path
 
+
 import xadmin
+from restShop.settings import MEDIA_ROOT
+from django.views.static import serve
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
 ]
